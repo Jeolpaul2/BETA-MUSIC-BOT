590841df0f2e99fd88cfa75169038b660cb8be31
BETA official <104895694+Jeolpaul2@users.noreply.github.com>
2022-05-09 08:43:40 +0530

    Update __main__.py

--- BetaMusic/__main__.py
+++ BetaMusic/__main__.py
@@ -18,10 +18,10 @@ from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
 from config import BANNED_USERS
-from YukkiMusic import LOGGER, app, userbot
-from YukkiMusic.core.call import Yukki
-from YukkiMusic.plugins import ALL_MODULES
-from YukkiMusic.utils.database import get_banned_users, get_gbanned
+from BetaMusic import LOGGER, app, userbot
+from BetaMusic.core.call import Beta
+from BetaMusic.plugins import ALL_MODULES
+from BetaMusic.utils.database import get_banned_users, get_gbanned
 
 loop = asyncio.get_event_loop()
 
@@ -34,7 +34,7 @@ async def init():
         and not config.STRING4
         and not config.STRING5
     ):
-        LOGGER("YukkiMusic").error(
+        LOGGER("BetaMusic").error(
             "No Assistant Clients Vars Defined!.. Exiting Process."
         )
         return
@@ -42,7 +42,7 @@ async def init():
         not config.SPOTIFY_CLIENT_ID
         and not config.SPOTIFY_CLIENT_SECRET
     ):
-        LOGGER("YukkiMusic").warning(
+        LOGGER("BetaMusic").warning(
             "No Spotify Vars defined. Your bot won't be able to play spotify queries."
         )
     try:
@@ -57,13 +57,13 @@ async def init():
     await app.start()
     for all_module in ALL_MODULES:
         importlib.import_module("BetaMusic.plugins" + all_module)
-    LOGGER("Yukkimusic.plugins").info(
+    LOGGER("BetaMusic.plugins").info(
         "Successfully Imported Modules "
     )
     await userbot.start()
-    await Yukki.start()
+    await Beta.start()
     try:
-        await Yukki.stream_call(
+        await Beta.stream_call(
             "http://docs.evostream.com/sample_content/assets/sintel1m720p.mp4"
         )
     except NoActiveGroupCall:
@@ -73,11 +73,11 @@ async def init():
         sys.exit()
     except:
         pass
-    await Yukki.decorators()
-    LOGGER("YukkiMusic").info("Beta Music Bot Started Successfully")
+    await Beta.decorators()
+    LOGGER("BetaMusic").info("Beta Music Bot Started Successfully")
     await idle()
 
 
 if __name__ == "__main__":
     loop.run_until_complete(init())
-    LOGGER("YukkiMusic").info("Stopping Beta Music Bot! GoodBye")
+    LOGGER("BetaMusic").info("Stopping Beta Music Bot! GoodBye")
