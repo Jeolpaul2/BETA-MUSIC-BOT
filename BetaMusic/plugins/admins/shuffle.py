45a817f767db47fe1d016f0760de94c78a7ffc86
BETA official <104895694+Jeolpaul2@users.noreply.github.com>
2022-05-09 11:25:14 +0530

    Create shuffle.py

---
+++ BetaMusic/plugins/admins/shuffle.py
@@ -0,0 +1,52 @@
+#
+# Copyright 2021-2022 Jeolpaul2
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# All rights reserved.
+
+import random
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from config import BANNED_USERS
+from strings import get_command
+from BetaMusic import app
+from BetaMusic.misc import db
+from BetaMusic.utils.decorators import AdminRightsCheck
+
+# Commands
+SHUFFLE_COMMAND = get_command("SHUFFLE_COMMAND")
+
+
+@app.on_message(
+    filters.command(SHUFFLE_COMMAND)
+    & filters.group
+    & ~filters.edited
+    & ~BANNED_USERS
+)
+@AdminRightsCheck
+async def admins(Client, message: Message, _, chat_id):
+    if not len(message.command) == 1:
+        return await message.reply_text(_["general_2"])
+    check = db.get(chat_id)
+    if not check:
+        return await message.reply_text(_["admin_21"])
+    try:
+        popped = check.pop(0)
+    except:
+        return await message.reply_text(_["admin_22"])
+    check = db.get(chat_id)
+    if not check:
+        check.insert(0, popped)
+        return await message.reply_text(_["admin_22"])
+    random.shuffle(check)
+    check.insert(0, popped)
+    await message.reply_text(
+        _["admin_23"].format(message.from_user.first_name)
+    )
