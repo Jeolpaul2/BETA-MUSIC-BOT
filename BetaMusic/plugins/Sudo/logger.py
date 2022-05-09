a8a1cf099121ef8120059f35fe2bb7453520f1f7
BETA official <104895694+Jeolpaul2@users.noreply.github.com>
2022-05-09 09:17:00 +0530

    Create logger.py

---
+++ BetaMusic/plugins/Sudo/logger.py
@@ -0,0 +1,40 @@
+#
+# Copyright 2021-2022 Jeolpaul2
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# All rights reserved.
+
+from pyrogram import filters
+
+import config
+from strings import get_command
+from BetaMusic import app
+from BetaMusic.misc import SUDOERS
+from BetaMusic.utils.database import add_off, add_on
+from BetaMusic.utils.decorators.language import language
+
+# Commands
+LOGGER_COMMAND = get_command("LOGGER_COMMAND")
+
+
+@app.on_message(filters.command(LOGGER_COMMAND) & SUDOERS)
+@language
+async def logger(client, message, _):
+    usage = _["log_1"]
+    if len(message.command) != 2:
+        return await message.reply_text(usage)
+    state = message.text.split(None, 1)[1].strip()
+    state = state.lower()
+    if state == "enable":
+        await add_on(config.LOG)
+        await message.reply_text(_["log_2"])
+    elif state == "disable":
+        await add_off(config.LOG)
+        await message.reply_text(_["log_3"])
+    else:
+        await message.reply_text(usage)
