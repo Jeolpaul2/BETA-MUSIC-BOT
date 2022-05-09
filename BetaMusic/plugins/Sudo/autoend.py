46b0728e270123db085f473340104665f9cb390a
BETA official <104895694+Jeolpaul2@users.noreply.github.com>
2022-05-09 09:04:58 +0530

    Create autoend.py

---
+++ BetaMusic/plugins/Sudo/autoend.py
@@ -0,0 +1,41 @@
+#
+# Copyright 2021-2022 Jeolpaul2
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# All rights reserved.
+
+from pyrogram import filters
+
+import config
+from strings import get_command
+from BetaMusic import app
+from BetaMusic.misc import SUDOERS
+from BetaMusic.utils.database import autoend_off, autoend_on
+from BetaMusic.utils.decorators.language import language
+
+# Commands
+AUTOEND_COMMAND = get_command("AUTOEND_COMMAND")
+
+
+@app.on_message(filters.command(AUTOEND_COMMAND) & SUDOERS)
+async def auto_end_stream(client, message):
+    usage = "**Usage:**\n\n/autoend [enable|disable]"
+    if len(message.command) != 2:
+        return await message.reply_text(usage)
+    state = message.text.split(None, 1)[1].strip()
+    state = state.lower()
+    if state == "enable":
+        await autoend_on()
+        await message.reply_text(
+            "Auto End Stream Enabled.\n\nBot will leave voice chat automatically after 3 mins if no one is listening with a warning message.."
+        )
+    elif state == "disable":
+        await autoend_off()
+        await message.reply_text("Auto End Stream Disabled.")
+    else:
+        await message.reply_text(usage)
