5f3642345deb8fb9beaf22fcb8804934d72245e4
BETA official <104895694+Jeolpaul2@users.noreply.github.com>
2022-05-09 12:42:38 +0530

    Create mongo.py

---
+++ BetaMusic/core/mongo.py
@@ -0,0 +1,45 @@
+#
+# Copyright 2021-2022 Jeolpaul2
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# All rights reserved.
+
+from motor.motor_asyncio import AsyncIOMotorClient as _mongo_client_
+from pymongo import MongoClient
+from pyrogram import Client
+
+import config
+
+from ..logging import LOGGER
+
+TEMP_MONGODB = "mongodb+srv://shikhar:shikhar@cluster0.6xzlh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+
+if config.MONGO_DB_URI is None:
+    LOGGER(__name__).warning(
+        "No MONGO DB URL found.. Your Bot will work on Beta's Database"
+    )
+    temp_client = Client(
+        "Yukki",
+        bot_token=config.BOT_TOKEN,
+        api_id=config.API_ID,
+        api_hash=config.API_HASH,
+    )
+    temp_client.start()
+    info = temp_client.get_me()
+    username = info.username
+    temp_client.stop()
+    _mongo_async_ = _mongo_client_(TEMP_MONGODB)
+    _mongo_sync_ = MongoClient(TEMP_MONGODB)
+    mongodb = _mongo_async_[username]
+    pymongodb = _mongo_sync_[username]
+else:
+    _mongo_async_ = _mongo_client_(config.MONGO_DB_URI)
+    _mongo_sync_ = MongoClient(config.MONGO_DB_URI)
+    mongodb = _mongo_async_.Beta
+    pymongodb = _mongo_sync_.Beta
