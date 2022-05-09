242849dfaff8f3acd77588bdb6cb0990eca6e4dd
sɴɪᴠɪɴ ☯︎ <98142697+snivinnn@users.noreply.github.com>
2022-05-09 10:16:55 +0530

    Create live.py

---
+++ BetaMusic/plugins/play/live.py
@@ -0,0 +1,79 @@
+#
+# Copyright 2021-2022 Jeolpaul2
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# All rights reserved.
+
+from pyrogram import filters
+
+from config import BANNED_USERS
+from BegaMusic import YouTube, app
+from BetaMusic.utils.channelplay import get_channeplayCB
+from BetaMusic.utils.decorators.language import languageCB
+from BetaMusic.utils.stream.stream import stream
+
+
+@app.on_callback_query(filters.regex("LiveStream") & ~BANNED_USERS)
+@languageCB
+async def play_live_stream(client, CallbackQuery, _):
+    callback_data = CallbackQuery.data.strip()
+    callback_request = callback_data.split(None, 1)[1]
+    vidid, user_id, mode, cplay, fplay = callback_request.split("|")
+    if CallbackQuery.from_user.id != int(user_id):
+        try:
+            return await CallbackQuery.answer(
+                _["playcb_1"], show_alert=True
+            )
+        except:
+            return
+    try:
+        chat_id, channel = await get_channeplayCB(
+            _, cplay, CallbackQuery
+        )
+    except:
+        return
+    video = True if mode == "v" else None
+    user_name = CallbackQuery.from_user.first_name
+    await CallbackQuery.message.delete()
+    try:
+        await CallbackQuery.answer()
+    except:
+        pass
+    mystic = await CallbackQuery.message.reply_text(
+        _["play_2"].format(channel) if channel else _["play_1"]
+    )
+    try:
+        details, track_id = await YouTube.track(vidid, True)
+    except Exception:
+        return await mystic.edit_text(_["play_3"])
+    ffplay = True if fplay == "f" else None
+    if not details["duration_min"]:
+        try:
+            await stream(
+                _,
+                mystic,
+                user_id,
+                details,
+                chat_id,
+                user_name,
+                CallbackQuery.message.chat.id,
+                video,
+                streamtype="live",
+                forceplay=ffplay,
+            )
+        except Exception as e:
+            ex_type = type(e).__name__
+            err = (
+                e
+                if ex_type == "AssistantErr"
+                else _["general_3"].format(ex_type)
+            )
+            return await mystic.edit_text(err)
+    else:
+        return await mystic.edit_text("Not a live stream")
+    await mystic.delete()
