8f08910c32cef37c957ac643f201a5cd9956b6d6
BETA official <104895694+Jeolpaul2@users.noreply.github.com>
2022-05-09 12:39:58 +0530

    Create dir.py

---
+++ BetaMusic/core/dir.py
@@ -0,0 +1,35 @@
+dir.py#
+# Copyright 2021-2022 Jeolpaul2
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# All rights reserved.
+
+import os
+import sys
+from os import listdir, mkdir
+
+from ..logging import LOGGER
+
+
+def dirr():
+    if "assets" not in listdir():
+        LOGGER(__name__).warning(
+            f"Assets Folder not Found. Please clone repository again."
+        )
+        sys.exit()
+    for file in os.listdir():
+        if file.endswith(".jpg"):
+            os.remove(file)
+    for file in os.listdir():
+        if file.endswith(".jpeg"):
+            os.remove(file)
+    if "downloads" not in listdir():
+        mkdir("downloads")
+    if "cache" not in listdir():
+        mkdir("cache")
+    LOGGER(__name__).info("Directories Updated.")
