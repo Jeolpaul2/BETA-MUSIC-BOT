91e67d91503e48ac7879c8c0d30c80838024371b
BETA official <104895694+Jeolpaul2@users.noreply.github.com>
2022-05-09 11:18:20 +0530

    Create pause.py

---
+++ BetaMusic/plugins/admins/pause.py
@@ -0,0 +1,42 @@
+#
+# Copyright 2021-2022 Jeolpaul2
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# All rights reserved.
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from config import BANNED_USERS
+from strings import get_command
+from BetaMusic import app
+from BetaMusic.core.call import Yukki
+from BetaMusic.utils.database import is_music_playing, music_off
+from BetaMusic.utils.decorators import AdminRightsCheck
+
+# Commands
+PAUSE_COMMAND = get_command("PAUSE_COMMAND")
+
+
+@app.on_message(
+    filters.command(PAUSE_COMMAND)
+    & filters.group
+    & ~filters.edited
+    & ~BANNED_USERS
+)
+@AdminRightsCheck
+async def pause_admin(cli, message: Message, _, chat_id):
+    if not len(message.command) == 1:
+        return await message.reply_text(_["general_2"])
+    if not await is_music_playing(chat_id):
+        return await message.reply_text(_["admin_1"])
+    await music_off(chat_id)
+    await Yukki.pause_stream(chat_id)
+    await message.reply_text(
+        _["admin_2"].format(message.from_user.mention)
+    )
