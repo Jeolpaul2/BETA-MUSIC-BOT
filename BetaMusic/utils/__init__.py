a9dc848086ce4f306118f46c08adba84582a87b3
BETA official <104895694+Jeolpaul2@users.noreply.github.com>
2022-05-09 11:58:35 +0530

    Create __init__.py

---
+++ BetaMusic/utils/__init__.py
@@ -0,0 +1,18 @@
+#
+# Copyright 2021-2022 Jeolpaul2
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# All rights reserved.
+
+from .channelplay import *
+from .database import *
+from .decorators import *
+from .formatters import *
+from .inline import *
+from .pastebin import *
+from .sys import *
