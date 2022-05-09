1128417c65d843bad15ecf6e42e470cdbe42827c
BETA official <104895694+Jeolpaul2@users.noreply.github.com>
2022-05-09 10:30:07 +0530

    Create videomode.py

---
+++ BetaMusic/plugins/Sudo/videomode.py
@@ -0,0 +1,41 @@
+#
+# Copyright 2021-2022 Jeolpaul2
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# All rights reserved.
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+import config
+from strings import get_command
+from BetaMusic import app
+from BetaMusic.misc import SUDOERS
+from BetaMusic.utils.database import add_off, add_on
+from BetaMusic.utils.decorators.language import language
+
+# Commands
+VIDEOMODE_COMMAND = get_command("VIDEOMODE_COMMAND")
+
+
+@app.on_message(filters.command(VIDEOMODE_COMMAND) & SUDOERS)
+@language
+async def videoloaymode(client, message: Message, _):
+    usage = _["vidmode_1"]
+    if len(message.command) != 2:
+        return await message.reply_text(usage)
+    state = message.text.split(None, 1)[1].strip()
+    state = state.lower()
+    if state == "download":
+        await add_on(config.YTDOWNLOADER)
+        await message.reply_text(_["vidmode_2"])
+    elif state == "m3u8":
+        await add_off(config.YTDOWNLOADER)
+        await message.reply_text(_["vidmode_3"])
+    else:
+        await message.reply_text(usage)
