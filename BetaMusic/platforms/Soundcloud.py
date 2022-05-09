f5ae4290c9ee60e1b9d8272a5bf3514886729a5e
BETA official <104895694+Jeolpaul2@users.noreply.github.com>
2022-05-09 11:54:53 +0530

    Update Soundcloud.py

--- BetaMusic/platforms/Soundcloud.py
+++ BetaMusic/platforms/Soundcloud.py
@@ -14,7 +14,7 @@ from os import path
 
 from yt_dlp import YoutubeDL
 
-from YukkiMusic.utils.formatters import seconds_to_min
+from BetaMusic.utils.formatters import seconds_to_min
 
 
 class SoundAPI:
