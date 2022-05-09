e1deee00b14fd0fa491c957572c0c9099d971a84
BETA official <104895694+Jeolpaul2@users.noreply.github.com>
2022-05-09 12:04:07 +0530

    Create logger.py

---
+++ BetaMusic/utils/logger.py
@@ -0,0 +1,41 @@
+#
+# Copyright 2021-2022 Jeolpaul2
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# All rights reserved.
+
+from config import LOG, LOG_GROUP_ID
+from BetaMusic import app
+from BetaMusic.utils.database import is_on_off
+
+
+async def play_logs(message, streamtype):
+    if await is_on_off(LOG):
+        if message.chat.username:
+            chatusername = f"@{message.chat.username}"
+        else:
+            chatusername = "Private Group"
+        logger_text = f"""
+**YUKKI PLAY LOG**
+**Chat:** {message.chat.title} [`{message.chat.id}`]
+**User:** {message.from_user.mention}
+**Username:** @{message.from_user.username}
+**User ID:** `{message.from_user.id}`
+**Chat Link:** {chatusername}
+**Query:** {message.text}
+**StreamType:** {streamtype}"""
+        if message.chat.id != LOG_GROUP_ID:
+            try:
+                await app.send_message(
+                    LOG_GROUP_ID,
+                    f"{logger_text}",
+                    disable_web_page_preview=True,
+                )
+            except:
+                pass
+        return
