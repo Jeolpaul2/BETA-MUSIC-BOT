e00d8413e92ddb8c96263e42c2fc134c5d2c1a35
BETA official <104895694+Jeolpaul2@users.noreply.github.com>
2022-05-09 11:31:25 +0530

    Create stop.py

---
+++ BetaMusic/plugins/admins/stop.py
@@ -0,0 +1,40 @@
+#
+# Copyright 2021-2022 Jeolpaul2
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# All rights reserved.
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from config import BANNED_USERS
+from strings import get_command
+from BetaMusic import app
+from BetaMusic.core.call import Yukki
+from BetaMusic.utils.database import set_loop
+from BetaMusic.utils.decorators import AdminRightsCheck
+
+# Commands
+STOP_COMMAND = get_command("STOP_COMMAND")
+
+
+@app.on_message(
+    filters.command(STOP_COMMAND)
+    & filters.group
+    & ~filters.edited
+    & ~BANNED_USERS
+)
+@AdminRightsCheck
+async def stop_music(cli, message: Message, _, chat_id):
+    if not len(message.command) == 1:
+        return await message.reply_text(_["general_2"])
+    await Yukki.stop_stream(chat_id)
+    await set_loop(chat_id, 0)
+    await message.reply_text(
+        _["admin_9"].format(message.from_user.mention)
+    )
