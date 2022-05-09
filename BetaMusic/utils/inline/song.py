cdf9457bb27eaca48b42d3a4acd4678070169d9b
BETA official <104895694+Jeolpaul2@users.noreply.github.com>
2022-05-09 12:18:59 +0530

    Create song.py

---
+++ BetaMusic/utils/inline/song.py
@@ -0,0 +1,33 @@
+#
+# Copyright 2021-2022 Jeolpaul2
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# All rights reserved.
+
+from pyrogram.types import InlineKeyboardButton
+
+
+def song_markup(_, vidid):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=_["SG_B_2"],
+                callback_data=f"song_helper audio|{vidid}",
+            ),
+            InlineKeyboardButton(
+                text=_["SG_B_3"],
+                callback_data=f"song_helper video|{vidid}",
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text=_["CLOSE_BUTTON"], callback_data="close"
+            ),
+        ],
+    ]
+    return buttons
