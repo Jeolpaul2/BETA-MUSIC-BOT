ae19e68073ce0fc22601c0608b938e28202d340b
BETA official <104895694+Jeolpaul2@users.noreply.github.com>
2022-05-09 10:41:46 +0530

    Create seeker.py

---
+++ BetaMusic/plugins/misc/seeker.py
@@ -0,0 +1,37 @@
+#
+# Copyright 2021-2022 Jeolpaul2
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# All rights reserved.
+
+import asyncio
+
+from BetaMusic.misc import db
+from BetaMusic.utils.database import (get_active_chats,
+                                       is_music_playing)
+
+
+async def timer():
+    while not await asyncio.sleep(1):
+        active_chats = await get_active_chats()
+        for chat_id in active_chats:
+            if not await is_music_playing(chat_id):
+                continue
+            playing = db.get(chat_id)
+            if not playing:
+                continue
+            file_path = playing[0]["file"]
+            if "index_" in file_path or "live_" in file_path:
+                continue
+            duration = int(playing[0]["seconds"])
+            if duration == 0:
+                continue
+            db[chat_id][0]["played"] += 1
+
+
+asyncio.create_task(timer())
